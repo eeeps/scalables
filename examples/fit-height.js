afa378e4f0e5e8b2ffd8e16ef593d84307d323de
Eric Portis <eric.portis@gmail.com>
2013-04-27 12:11:02 -0600

    tweaks...

--- examples/fit-height.js
+++ examples/fit-height.js
@@ -28,27 +28,27 @@ Modernizr.addTest('img-pct-height', function(){
 
 (function() {
 
-var fitHeight = function() {
-
-	var els = document.querySelectorAll('.photo');
-	for ( var i = 0, len = els.length; i < len; i++ ) {
-
-		var img = els[i].getElementsByTagName('img')[0];
-		var imgRatio = parseInt(img.getAttribute('data-width')) / parseInt(img.getAttribute('data-height'));
-		
-		var constrainingRatio = window.innerWidth / (window.innerHeight * .9);
-
-		if (imgRatio < constrainingRatio) {
-			els[i].classList.add('tall');
-		} else {
-			els[i].classList.remove('tall');
+	var fitHeight = function() {
+	
+		var els = document.querySelectorAll('.photo');
+		for ( var i = 0, len = els.length; i < len; i++ ) {
+	
+			var img = els[i].getElementsByTagName('img')[0];
+			var imgRatio = parseInt(img.getAttribute('data-width')) / parseInt(img.getAttribute('data-height'));
+			
+			var constrainingRatio = window.innerWidth / (window.innerHeight * .9);
+	
+			if (imgRatio < constrainingRatio) {
+				els[i].classList.add('tall');
+			} else {
+				els[i].classList.remove('tall');
+			}
 		}
 	}
-}
-
-fitHeight();
-
-window.addEventListener('resize', fitHeight, false);
-window.addEventListener('orientationchange', fitHeight, false);
+	
+	fitHeight();
+	
+	window.addEventListener('resize', fitHeight, false);
+	window.addEventListener('orientationchange', fitHeight, false);
 
 })();
